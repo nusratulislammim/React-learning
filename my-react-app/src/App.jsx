00410ab05749adec55b6d8be8105b8ab57592372
Nusratul Islam Mim <nusratulislammim@gmail.com>
2025-10-29 12:14:51 +0600

    create a ”Hello World” Re- act app

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,10 @@
+function App(){
+  return(
+    <div style={{textAlign: 'center',  marginTop: '50px'}}>
+      <h1>Hello World!</h1>
+      <p>Welcome to React</p>
+    </div>
+  )
+}
+
+export default App
