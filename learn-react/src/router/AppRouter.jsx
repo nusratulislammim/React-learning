399982e701f01d4e0edbe1de35b7d1bd791a1716
Nusratul Islam Mim <nusratulislammim@gmail.com>
2025-11-02 17:59:32 +0600

    update

--- learn-react/src/router/AppRouter.jsx
+++ learn-react/src/router/AppRouter.jsx
@@ -1,6 +1,7 @@
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-import Day01 from '../pages/day-01';
-import Day02 from '../pages/day-02';
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Day01 from "../pages/day-01";
+import Day02 from "../pages/day-02";
+import Day03 from "../pages/day-03";
 
 const AppRouter = () => {
   return (
@@ -9,6 +10,7 @@ const AppRouter = () => {
         <Route path="/" element={<Navigate to="/day-01" />} />
         <Route path="/day-01/*" element={<Day01 />} />
         <Route path="/day-02/*" element={<Day02 />} />
+        <Route path="/day-03/*" element={<Day03 />} />
       </Routes>
     </BrowserRouter>
   );
