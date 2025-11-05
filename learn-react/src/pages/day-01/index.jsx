826d136fb9f2dec36e79a5c6bbb62f94c3785458
Nusratul Islam Mim <nusratulislammim@gmail.com>
2025-11-05 20:15:30 +0600

    Update index.jsx

--- learn-react/src/pages/day-01/index.jsx
+++ learn-react/src/pages/day-01/index.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-import One from "./One";
+import One from "..day-01/One";
 
 const Day01 = () => {
   return (
     <div>
       <h2>Day 01 Exercises</h2>
-      <One name="Mim" />
+      <One />
     </div>
   );
 };
