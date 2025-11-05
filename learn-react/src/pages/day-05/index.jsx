64e90c8344f4e24da01f7758dfe3a4ebaccdbafa
Nusratul Islam Mim <nusratulislammim@gmail.com>
2025-11-05 21:03:53 +0600

    Create index.jsx

---
+++ learn-react/src/pages/day-05/index.jsx
@@ -0,0 +1,13 @@
+import React  from "react";
+import One from "../day-05/One";
+
+const Day05 = () =>{
+    return(
+        <div>
+        <h2>Day-05: State and Lifecycle </h2>
+        <One/>
+        </div>
+    );
+};
+
+export default Day05;
