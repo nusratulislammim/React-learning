588442ca68e788d967ca6e3d071be0851a21f015
Nusratul Islam Mim <nusratulislammim@gmail.com>
2025-11-06 10:37:17 +0600

    Create One.jsx

---
+++ learn-react/src/pages/day-06/One.jsx
@@ -0,0 +1,18 @@
+import React, {useState} from "react";
+
+function ClickMe (){
+    const [text, setText] = useState("Before Click");
+
+    function handleClick(){
+        setText("Button has been clicked");
+    }
+    return(
+        <div style={{backgroundColor:"skyblue", textAlign:"center"}}>
+            <h2>click the button</h2>
+            <h3>{text}</h3>
+            <button onClick={handleClick}>Click Me</button>
+
+        </div>
+    );
+};
+export default ClickMe;
