d383872a2fe16664646707b65db55557e3a862ec
Nusratul Islam Mim <nusratulislammim@gmail.com>
2025-11-06 11:03:22 +0600

    Create index.jsx

---
+++ learn-react/src/pages/day-07/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import One from "../day-07/One";
+
+function Day07(){
+    return(
+        <div>
+            <h1>Day-07: Conditional Rendering</h1>
+            <h2>Task: Show ”Login” / ”Logout” based on a boolean state</h2>
+            <One />
+        </div>
+    );
+};
+
+export default Day07;
