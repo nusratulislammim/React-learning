b78a0b03b3542bbc305b359bc93c2743d2e618a1
Nusratul Islam Mim <nusratulislammim@gmail.com>
2025-11-06 11:03:29 +0600

    Update One.jsx

--- learn-react/src/pages/practice/One.jsx
+++ learn-react/src/pages/practice/One.jsx
@@ -1,16 +1,16 @@
 import React, {useState} from "react";
 
 function ClickMe (){
-    const [text, setText] = useState("Before Click");
+    const [a, set] = useState("Before Click");
 
     function handleClick(){
-        setText("Button has been clicked");
+        set("Button has been clicked");
     }
     return(
         <div>
             <h2>click the button</h2>
-            <h3>{text}</h3>
-            <button onClick={handleClick}>Click Me</button>
+            <h3>{a}</h3>
+            <button onClick={()=>{set("hi")}}>Click Me</button>
 
         </div>
     );
