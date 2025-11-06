2f8e039829731355d448759c34a5947eb0c0eae7
Nusratul Islam Mim <nusratulislammim@gmail.com>
2025-11-06 10:37:21 +0600

    Update index.jsx

--- learn-react/src/pages/practice/index.jsx
+++ learn-react/src/pages/practice/index.jsx
@@ -2,15 +2,10 @@ import React from "react";
 import One from "../practice/One";
 
 function Practice(){
-    const student ={
-        name:"Nusratul Islam Meem",
-        age: 21,
-        department: "CSE",
-    }
     return(
         <div>
-            <h2>Day 04: Components and Props</h2>
-            <One info={student}/>
+            <h2>Practice</h2>
+            <One/>
         </div>
     );
 };
