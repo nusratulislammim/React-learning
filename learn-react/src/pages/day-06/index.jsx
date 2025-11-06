0dcc076f20c6e6ffd38fa00f0496be930542b909
Nusratul Islam Mim <nusratulislammim@gmail.com>
2025-11-06 10:37:13 +0600

    Create index.jsx

---
+++ learn-react/src/pages/day-06/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import One from "../day-06/One";
+
+function Day06 (){
+    return(
+        <div>
+            <h1>Day-06:Handling Events</h1>
+            <h2>Task: Build a button that changes text on click</h2>
+            <One /> 
+        </div>
+    );
+};
+
+export default Day06;
