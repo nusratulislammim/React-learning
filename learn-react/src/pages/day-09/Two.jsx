7796d398bd8d3869d7f3a1c6e759b0875728438b
Nusratul Islam Mim <nusratulislammim@gmail.com>
2025-11-07 11:22:23 +0600

    Create Two.jsx

---
+++ learn-react/src/pages/day-09/Two.jsx
@@ -0,0 +1,14 @@
+import React,{useState} from "react";
+
+function Form(){
+    const [name, setName] = useState("");
+
+    return(
+        <div>
+            <input type="text" value={name} onChange={(e)=> setName(e.target.value)} />
+            <p>You Typed: {name}</p>
+        </div>
+    );
+};
+
+export default Form;
