551c33752cc21e72263ca3d7d1cc30f98cd0b398
Nusratul Islam Mim <nusratulislammim@gmail.com>
2025-11-07 11:22:13 +0600

    Create index.jsx

---
+++ learn-react/src/pages/day-09/index.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import One from "../day-09/One";
+import Two from "../day-09/Two";
+
+function Day09(){
+    return(
+        <div>
+            <h1>Day-09: Controlled Inputs & Forms</h1>
+            <h2>Task: Build a simple form with input & submit button</h2>
+            <Two/>
+            <One/>
+        </div>
+    );
+};
+
+export default Day09;
