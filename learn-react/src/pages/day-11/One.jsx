cbd9818e2477dc2b4d8d17d988f2ccaa16346dbe
Nusratul Islam Mim <nusratulislammim@gmail.com>
2025-11-15 18:23:41 +0600

    Create One.jsx

---
+++ learn-react/src/pages/day-11/One.jsx
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+import ChildA from "./Two";
+import ChildB from "./three";
+
+function Parent(){
+    const [text, setText] = useState("");
+    return(
+        <div>
+            <h2>Parent Component</h2>
+
+            <ChildA text= {text} setText={setText}/>
+            <ChildB text={text}/>
+        </div>
+    );
+}
+
+export default Parent;
