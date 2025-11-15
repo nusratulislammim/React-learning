77e01e3161fb9661e0e4caec332d81ba9ea7d409
Nusratul Islam Mim <nusratulislammim@gmail.com>
2025-11-15 18:23:37 +0600

    Create index.jsx

---
+++ learn-react/src/pages/day-11/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import One from "../day-11/One";
+
+function Day11 (){
+    return(
+        <div>
+            <h1>Lifting State Up</h1>
+            <h2>task: Share state between sibling components via parent</h2>
+            <One/>
+        </div>
+    );
+}
+export default Day11;
