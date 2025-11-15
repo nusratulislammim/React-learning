de8d5f07df9ce606136af29c1718c410e0f2fe66
Nusratul Islam Mim <nusratulislammim@gmail.com>
2025-11-15 18:23:31 +0600

    Update index.jsx

---
+++ learn-react/src/pages/day-10/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import One from '../day-10/One';
+
+function Day10(){
+    return(
+        <div>
+            <h1>Day-10: Uncontrolled Inputs & Refs</h1>
+            <h2>task: Focus an input on button click using useRef</h2>
+            <One/>
+        </div>
+    );
+}
+
+export default Day10;
