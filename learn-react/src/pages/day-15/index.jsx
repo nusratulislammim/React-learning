396d1eb328e6aadb6cdba37feaa94623847fe617
Nusratul Islam Mim <nusratulislammim@gmail.com>
2025-11-20 17:27:03 +0600

    Create index.jsx

---
+++ learn-react/src/pages/day-15/index.jsx
@@ -0,0 +1,12 @@
+import TimerComponent from "./TimeComponent";
+
+function Day15(){
+    return(
+        <div>
+            <h1>Day-15</h1>
+            <h2>task: Create a timer with cleanup on unmount </h2>
+            <TimerComponent/>
+        </div>
+    );
+}
+export default Day15;
