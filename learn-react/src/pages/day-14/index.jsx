fd085a148802cd93d74f2816d78dba57f7f3a019
Nusratul Islam Mim <nusratulislammim@gmail.com>
2025-11-20 17:27:07 +0600

    Create index.jsx

---
+++ learn-react/src/pages/day-14/index.jsx
@@ -0,0 +1,12 @@
+import UseEffectUse from "./App";
+
+function Day14(){
+    return(
+        <div>
+            <h1>Day-14</h1>
+            <h2>task: Fetch data from a public API and display it </h2>
+            <UseEffectUse/>
+        </div>
+    );
+}
+export default Day14;
