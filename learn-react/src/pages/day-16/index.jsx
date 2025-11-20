549859f98d7349ff38853ef616ebf6192f5f7450
Nusratul Islam Mim <nusratulislammim@gmail.com>
2025-11-20 17:26:51 +0600

    Create index.jsx

---
+++ learn-react/src/pages/day-16/index.jsx
@@ -0,0 +1,13 @@
+import AppToggle from "./AppToggle";
+
+
+function Day16(){
+    return(
+        <div>
+            <h1>Day-16: Custom Hooks</h1>
+            <h2>task: Make a hook that toggles a boolean or fetches API data</h2>
+            <AppToggle/>
+        </div>
+    );
+}
+export default Day16;
