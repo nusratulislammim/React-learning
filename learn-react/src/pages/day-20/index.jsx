f45a20617d48429f029e1674245caa571ed5e2fd
Nusratul Islam Mim <nusratulislammim@gmail.com>
2025-11-22 12:15:35 +0600

    Create index.jsx

---
+++ learn-react/src/pages/day-20/index.jsx
@@ -0,0 +1,12 @@
+import Parent from "./Parent";
+
+function Day20(){
+    return(
+        <div>
+            <h1>Day-20: useImperativeHandle</h1>
+            <h2>task:Expose child function to parent using useImperativeHandle </h2>
+            <Parent/>
+        </div>
+    );
+}
+export default Day20;
