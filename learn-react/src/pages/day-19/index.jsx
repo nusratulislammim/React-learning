808344dd990b3251bf3ebe60e36fd94e066af61e
Nusratul Islam Mim <nusratulislammim@gmail.com>
2025-11-22 11:57:53 +0600

    Create index.jsx

---
+++ learn-react/src/pages/day-19/index.jsx
@@ -0,0 +1,12 @@
+import Parent from "./Parent";
+
+function Day19(){
+    return(
+        <div>
+            <h1>Day-19: useRef & Forwarding Refs </h1>
+            <h2>Task: Focus input via ref, forward ref to child component </h2>
+            <Parent/>
+        </div>
+    );
+}
+export default Day19;
