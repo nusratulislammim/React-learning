30f69a89e3de70717b9af182baaab6607fb14f26
Nusratul Islam Mim <nusratulislammim@gmail.com>
2025-11-22 11:35:50 +0600

    Update index.jsx

--- learn-react/src/pages/day-17/index.jsx
+++ learn-react/src/pages/day-17/index.jsx
@@ -4,8 +4,8 @@ import ThemeProvider from "./ThemeProvider";
 function Day17 (){
     return(
         <div>
-            <h1>Day-17: </h1>
-            <h2>task: </h2>
+            <h1>Day-17: Context </h1>
+            <h2>task: Create a theme context (light/dark mode) </h2>
             <ThemeProvider>
                 <ThemeButton/>
             </ThemeProvider>
