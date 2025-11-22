dc4fc6fe42fcac201955c57acf056511226caf82
Nusratul Islam Mim <nusratulislammim@gmail.com>
2025-11-22 11:35:57 +0600

    Create index.jsx

---
+++ learn-react/src/pages/day-18/index.jsx
@@ -0,0 +1,14 @@
+import UseReducerApp from "./UseReducerApp";
+import Counter from "./Counter";
+function Day18(){
+    return(
+        <div>
+            <h1>Day-18: useReducer Hook</h1>
+            <h2>task: Implement a counter using useReducer</h2>
+            <UseReducerApp/>
+            <Counter/>
+        </div>
+    );
+}
+
+export default Day18;
