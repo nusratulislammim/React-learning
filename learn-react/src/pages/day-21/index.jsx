298902b83b590c45a31d79a70538bc97ff8a4667
Nusratul Islam Mim <nusratulislammim@gmail.com>
2025-11-25 17:35:03 +0600

    Create index.jsx

---
+++ learn-react/src/pages/day-21/index.jsx
@@ -0,0 +1,13 @@
+import MeasureLayout from "./MeasureLayout";
+
+function Day21(){
+    return(
+        <div>
+            <h1>Day-21: useLayoutEffect </h1>
+            <h2>Task: Measure layout of DOM element after render</h2>
+            <MeasureLayout/>
+        </div>
+    );
+}
+
+export default Day21;
